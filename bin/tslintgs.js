a3c36360c768a17fb898b74fe31e2b61188e2420
howardchi <howardchi@fougood.com>
2017-06-28 19:08:36 +0800

    add install in global

---
+++ bin/tslintgs.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const args = require('args');
+const {resolve} = require('path');
+const chalk = require('chalk');
+const TslintGitStatus = require('../lib/index').default;
+
+args
+  .option('tslint', 'tslint.json file path', resolve(process.cwd(), './tslint.json'))
+  .option('git', 'your git directory, where your .git exist', resolve(process.cwd()))
+  .option('ext', 'extension names, can use multiple extensions seperate with comma', '.ts');
+
+const flags = args.parse(process.argv);
+
+new TslintGitStatus(flags.tslint, flags.git, flags.ext.split(',')).start()
+  .then(() => {
+    console.log(chalk.underline.green("tsLint all pass!"));
+  })
+  .catch(err => {
+    // err, when lint failed
+    process.exit(1);
+    throw new Error(err);
+  });
